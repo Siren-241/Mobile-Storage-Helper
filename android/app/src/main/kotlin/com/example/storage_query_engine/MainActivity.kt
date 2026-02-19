097f51a2f84f6a44362c3aedda23b87617d5f027
Siren-241 <ovalekarsaksham7@gmail.com>
2026-02-19 15:33:20 +0530

    Intial Commit

---
+++ android/app/src/main/kotlin/com/example/storage_query_engine/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.storage_query_engine
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:photo_manager/photo_manager.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return const MaterialApp(
+      home: MediaCounterScreen(),
+    );
+  }
+}
+
+class MediaCounterScreen extends StatefulWidget {
+  const MediaCounterScreen({super.key});
+
+  @override
+  State<MediaCounterScreen> createState() => _MediaCounterScreenState();
+}
+
+class _MediaCounterScreenState extends State<MediaCounterScreen> {
+  int imageCount = 0;
+
+  String status = "Requesting Permissions...";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _loadImages();
+  }
+
+  Future<void> _loadImages() async {
+    final permission = await PhotoManager.requestPermissionExtend();
+
+    if (!permission.isAuth) {
+      setState(() {
+        status = "Permission Denied";
+      });
+      return;
+    }
+
+    setState(() {
+      status = "Loading Images...";
+    });
+
+    final albums = await PhotoManager.getAssetPathList(
+      type: RequestType.image,
+    );
+
+    int total = 0;
+
+    for (final album in albums) {
+      final count = await album.assetCountAsync;
+      total += count;
+    }
+
+    setState(() {
+      imageCount = total;
+      status = "Done";
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Storage Helper")),
+      body: Center (
+        child: Text(
+          status == "Done"
+              ? "Found $imageCount images"
+              : status,
+
+          style: const TextStyle(fontSize: 22),
+          textAlign: TextAlign.center,
+        )
+      )
+    );
+  }
+}
